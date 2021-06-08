e1f4cc09f33e2e4302c893e3d9e07a260524b5a3
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-07 21:32:25 -0400

    passing with a weird error

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,43 +1,69 @@
 const Accounts = require('./accounts-model');
 
 exports.logger = (req, res, next) => {
-	// DO YOUR MAGIC
-	// console.log('logger: WORKING');
 	console.log(`[${new Date().toLocaleString()}] [${req.method}] ${req.path}`);
 	next();
 };
 
 exports.checkAccountPayload = (req, res, next) => {
-	// DO YOUR MAGIC
-	console.log('checkAccountPayload: WORKING');
-	Accounts.getAll() // FIX THIS
-		.then(thing => {
-			if (!thing) {
-				next();
-			} else {
-				next();
-			}
+	const name = req.body.name;
+	const budget = req.body.budget;
+
+	if (!req.body.name || (!req.body.budget && isNaN(req.body.budget))) {
+		next({
+			status: 400,
+			message: 'name and budget are required'
+		});
+	} else if (typeof name !== 'string') {
+		next({
+			status: 400,
+			message: 'name of account must be a string'
+		});
+	} else if (name.trim().length < 3 || name.trim().length > 100) {
+		next({
+			status: 400,
+			message: 'name of account must be between 3 and 100'
+		});
+	} else if (typeof budget !== 'number') {
+		next({
+			status: 400,
+			message: 'budget of account must be a number'
+		});
+	} else if (budget < 0 || budget > 1000000) {
+		next({
+			status: 400,
+			message: 'budget of account is too large or too small'
+		});
+	} else {
+		req.name = name.trim();
+		next();
+	}
+};
+
+exports.checkAccountNameUnique = (req, res, next) => {
+	Accounts.getAll()
+		.then(accounts => {
+			accounts.filter(account => {
+				if (account.name === req.body.name) {
+					next({
+						status: 400,
+						message: 'that name is taken'
+					});
+				}
+			});
+			next();
 		})
 		.catch(next);
 };
 
-exports.checkAccountNameUnique = (req, res, next) => {
-	// DO YOUR MAGIC
-	console.log('checkAccountNameUnique: WORKING');
-
-	next();
-};
-
 exports.checkAccountId = (req, res, next) => {
-	// DO YOUR MAGIC
-	// console.log('checkAccountId: WORKING');
 	Accounts.getById(req.params.id)
 		.then(account => {
 			if (!account) {
-				res.status(404).json({
+				next({
+					status: 404,
 					message: 'account not found'
 				});
-				next();
 			} else {
 				req.account = account;
 				next();
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -4,41 +4,55 @@ const { checkAccountPayload, checkAccountNameUnique, checkAccountId } = require(
 const Accounts = require('./accounts-model');
 
 router.get('/', (req, res, next) => {
-	// DO YOUR MAGIC
 	Accounts.getAll()
 		.then(accounts => {
-			// console.log('ACCOUNTS: ', accounts);
 			res.status(200).json(accounts);
 		})
 		.catch(next);
 });
 
 router.get('/:id', checkAccountId, (req, res, next) => {
-	// DO YOUR MAGIC
 	Accounts.getById(req.params.id)
 		.then(account => {
-			// console.log('ACCOUNT: ', account);
 			res.status(200).json(account);
 		})
 		.catch(next);
 });
 
-router.post('/', checkAccountPayload, (req, res, next) => {
-	// DO YOUR MAGIC
+router.post('/', checkAccountNameUnique, checkAccountPayload, (req, res, next) => {
+	Accounts.create({ name: req.name, budget: req.body.budget })
+		.then(account => {
+			res.status(201).json(account);
+		})
+		.catch(next);
 });
 
-router.put('/:id', checkAccountPayload, (req, res, next) => {
-	// DO YOUR MAGIC
+router.put('/:id', checkAccountId, checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
+	Accounts.updateById(req.params.id, req.body)
+		.then(updatedAccount => {
+			res.json(updatedAccount);
+		})
+		.catch(next);
+
+	// try {
+	// 	const data = await Accounts.updateById(req.params.id, req.body);
+	// 	res.json(data);
+	// } catch (err) {
+	// 	next(err);
+	// }
 });
 
-router.delete('/:id', (req, res, next) => {
-	// DO YOUR MAGIC
+router.delete('/:id', checkAccountId, (req, res, next) => {
+	Accounts.deleteById(req.params.id)
+		.then(deletedPost => {
+			res.status(200).json(deletedPost);
+		})
+		.catch(next);
 });
 
 // eslint-disable-line
 router.use((err, req, res, next) => {
-	// DO YOUR MAGIC
-	console.log('Err handling middleware kicking in!', err.message);
+	// console.log('Err handling middleware kicking in!', err.message);
 	res.status(err.status || 500).json({
 		custom: 'Strange things are afoot at the Circle K',
 		message: err.message,
